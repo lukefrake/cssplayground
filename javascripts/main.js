67c9f77e66ba10a9a02fc507ace542d723a61b00
dancork <daniel.cork@holidayextras.com>
2015-09-16 12:58:21 +0000

    Move gutter and improve jekyll

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,7 +1,20 @@
-$(document).ready(function () {
-  var editor = ace.edit('cssArea');
-  editor.getSession().setMode('ace/mode/css');
-  editor.on( 'blur', function( event ) {
-    $( '#cssDump' ).html( editor.getValue() );
-  } );
-});
+;(function(init) {
+
+  init(window.jQuery, window, document);
+
+}(function($, window, document) {
+
+  $(function() {
+
+    var editor = ace.edit('cssArea');
+    var $dumping_ground = $( '#cssDump' );
+
+    editor.getSession().setMode('ace/mode/css');
+
+    editor.on( 'change', function( event ) {
+      $dumping_ground.html( editor.getValue() );
+    });
+
+  });
+
+}));
